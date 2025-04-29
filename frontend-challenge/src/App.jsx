e293b4361edc2cdf4a2a036df1e8dd30ba6f6a06
Your Name <your.email@example.com>
2025-04-29 19:22:52 +0300

    set up tailwind

--- frontend-challenge/src/App.jsx
+++ frontend-challenge/src/App.jsx
@@ -2,17 +2,21 @@ import { Redirect, Route, Switch } from 'react-router-dom/cjs/react-router-dom.m
 import Login from './pages/Login'
 import Feed from './pages/Feed'
 
+
 function App() {
 
   return (
     <>
       <Switch>
+      <h1 className="text-3xl font-bold underline text-red-500">
+    Hello world!
+  </h1>
         <Route exact path="/" >
         <Redirect to="/login" />
         </Route>
         <Route path="/login" > <Login/>
         </Route>
-        <Route path="/feed"  > <Feed/>
+        <Route path="/feed" > <Feed/>
         </Route>
       </Switch>
     </>
