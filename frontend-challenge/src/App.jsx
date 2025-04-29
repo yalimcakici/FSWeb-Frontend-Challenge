eb507338b0450a6413fe59742308b50ee5f28b59
Your Name <your.email@example.com>
2025-04-29 18:43:42 +0300

    adds routing

--- frontend-challenge/src/App.jsx
+++ frontend-challenge/src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Redirect, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min'
+import Login from './pages/Login'
+import Feed from './pages/Feed'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Switch>
+        <Route exact path="/" >
+        <Redirect to="/login" />
+        </Route>
+        <Route path="/login" > <Login/>
+        </Route>
+        <Route path="/feed"  > <Feed/>
+        </Route>
+      </Switch>
     </>
   )
 }
